11e1a79b68f0bdc88064c2af39d62756693df6f0
504brandon <mb.bot.official@gmail.com>
2023-11-19 14:37:39 -0600

    beta bot

--- source/Options.hx
+++ source/Options.hx
@@ -159,6 +159,27 @@ class SongPositionOption extends Option
 	}
 }
 
+class BotOption extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+
+	public override function press():Bool
+	{
+		FlxG.save.data.bot = !FlxG.save.data.bot;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return FlxG.save.data.bot ? "BotPlay" : "PlayerPlay";
+	}
+}
+
 class Judgement extends Option
 {
 	
--- source/TitleState.hx
+++ source/TitleState.hx
@@ -53,7 +53,7 @@ class TitleState extends MusicBeatState
 		#if sys
 		polymod.Polymod.init({
 			modRoot: "mods",
-			dirs: [Assets.getText(Paths.txt("modSelected"))],
+			dirs: [Assets.getText(Paths.txt("modSelected")), "global"],
 			errorCallback: (e) -> {
 				trace(e.message);
 			},
