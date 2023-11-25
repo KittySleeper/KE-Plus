b5fe18d5d1a2443834ba88d07340e414057457df
BamBamLol <82470343+bambamlolol@users.noreply.github.com>
2023-11-24 23:56:59 -0800

    fix seziures maybe?? idk

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -175,8 +175,7 @@ class FreeplayState extends MusicBeatState
 			trace('CUR WEEK' + PlayState.storyWeek);
 			LoadingState.loadAndSwitchState(new PlayState());
 		}
-
-		bg.color = FlxColor.fromInt(Std.int(FlxMath.lerp(bg.color, songs[curSelected].songColor, 0.57)));
+		bg.color = FlxColor.lerp(bg.color, songs[curSelected].songColor, 0.5);
 	}
 
 	function changeDiff(change:Int = 0)
