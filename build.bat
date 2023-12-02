248de18283d7f7d797f4aeb61d7d5893af215b84
BamBamLol <82470343+bambamlolol@users.noreply.github.com>
2023-12-02 15:50:51 -0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 haxelib git hscript-improved https://www.github.com/YoshiCrafter29/hscript-improved
 haxelib git scriptless-polymod https://github.com/SrtHero278/scriptless-polymod
 haxelib install hxCodec
-lime test windows
+lime test windows 
