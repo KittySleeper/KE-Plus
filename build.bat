3e0a452cd552ae8923d7e1281ad1230ba8be0239
BamBamLol <82470343+bambamlolol@users.noreply.github.com>
2023-12-05 09:02:20 -0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,16 @@
+haxelib install lime 7.9.0
+haxelib install openfl
+haxelib install flixel
+haxelib run lime setup
+haxelib run lime setup flixel
+haxelib install flixel-tools
+haxelib run flixel-tools setup
+haxelib install flixel-addons
+haxelib install flixel-ui
+haxelib install newgrounds
+haxelib install linc_luajit
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git hscript-improved https://www.github.com/YoshiCrafter29/hscript-improved
 haxelib git scriptless-polymod https://github.com/SrtHero278/scriptless-polymod
 haxelib install hxCodec
