1a9ce05f725b00330451dfca60d96d36c52fd8c0
Bammmmmmmmmmmmm <lebaldiofficial@gmail.com>
2024-01-13 21:54:41 -0600

    fsdfsdfsd

--- source/states/game/PlayState.hx
+++ source/states/game/PlayState.hx
@@ -897,13 +897,11 @@ class PlayState extends MusicBeatState
 				}
 			default:
 				{
-					var stageExists = false;
 
 					for (allowed in HScript.allowedExtensions)
 					{
 						if (Assets.exists('assets/stages/${SONG.stage.toLowerCase()}.$allowed'))
 						{
-							stageExists = true;
 							var script = new HScript('assets/stages/${SONG.stage}');
 
 							if (!script.isBlank && script.expr != null)
@@ -917,34 +915,21 @@ class PlayState extends MusicBeatState
 							if (!scripts.contains(script))
 								scripts.push(script);
 						}
-					}
+						else
+						{
+							var script = new HScript('assets/stages/stage');
 
-					if (!stageExists)
-					{
-						defaultCamZoom = 0.9;
-						curStage = 'stage';
-						var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image('stageback'));
-						bg.antialiasing = true;
-						bg.scrollFactor.set(0.9, 0.9);
-						bg.active = false;
-						add(bg);
+							if (!script.isBlank && script.expr != null)
+							{
+								script.interp.scriptObject = this;
+								script.setValue('add', add);
+								script.setValue('remove', remove);
+								script.interp.execute(script.expr);
+							}
 
-						var stageFront:FlxSprite = new FlxSprite(-650, 600).loadGraphic(Paths.image('stagefront'));
-						stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
-						stageFront.updateHitbox();
-						stageFront.antialiasing = true;
-						stageFront.scrollFactor.set(0.9, 0.9);
-						stageFront.active = false;
-						add(stageFront);
-
-						var stageCurtains:FlxSprite = new FlxSprite(-500, -300).loadGraphic(Paths.image('stagecurtains'));
-						stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
-						stageCurtains.updateHitbox();
-						stageCurtains.antialiasing = true;
-						stageCurtains.scrollFactor.set(1.3, 1.3);
-						stageCurtains.active = false;
-
-						add(stageCurtains);
+							if (!scripts.contains(script))
+								scripts.push(script);
+						}
 					}
 				}
 		}
