c57ab9cd5fa216ef77796beb463f968d1ece8359
DemonDezerin <145730983+DemonDezerin@users.noreply.github.com>
2024-03-26 02:35:20 -0400

    hi

--- source/import.hx
+++ source/import.hx
@@ -1,49 +1,51 @@
-import Paths;
+import Paths;
 
-import states.LoadReplayState;
+import states.LoadReplayState;
 
-import states.game.PlayState;
+import states.game.PlayState;
 
-import states.mainmenu.FreeplayState;
-import states.mainmenu.StoryMenuState;
-import states.mainmenu.OptionsMenu;
+import states.mainmenu.FreeplayState;
+import states.mainmenu.StoryMenuState;
+import states.mainmenu.OptionsMenu;
 
-import objects.FunkinBar;
-import objects.HealthIcon;
-import objects.BackgroundGirls;
-import objects.BackgroundDancer;
-import objects.BGSprite;
-import objects.MenuItem;
-import objects.Alphabet;
-import objects.Note;
-import objects.DialogueBox;
-import objects.Character;
-import objects.MenuCharacter;
+import objects.FunkinBar;
+import objects.HealthIcon;
+import objects.BackgroundGirls;
+import objects.BackgroundDancer;
+import objects.BGSprite;
+import objects.MenuItem;
+import objects.Alphabet;
+import objects.Note;
+import objects.DialogueBox;
+import objects.Character;
+import objects.MenuCharacter;
+import objects.OverlayShader;
+import objects.BlendModeEffect;
 
 #if windows
-import backend.Discord.DiscordClient;
+import backend.Discord.DiscordClient;
 #end
-import backend.Song;
-import backend.MusicBeatState;
-import backend.MusicBeatSubstate;
-import backend.HScript;
-import backend.Controls;
-import backend.ControlsSubState;
-import backend.Conductor;
-import backend.Highscore;
-import backend.Replay;
-import backend.Ratings;
-import backend.EtternaFunctions;
-import backend.ChartParser;
-import backend.WiggleEffect;
-import backend.KadeEngineData;
-import backend.APIStuff;
-import backend.ConvertScore;
-import backend.CoolUtil;
+import backend.Song;
+import backend.MusicBeatState;
+import backend.MusicBeatSubstate;
+import backend.HScript;
+import backend.Controls;
+import backend.ControlsSubState;
+import backend.Conductor;
+import backend.Highscore;
+import backend.Replay;
+import backend.Ratings;
+import backend.EtternaFunctions;
+import backend.ChartParser;
+import backend.WiggleEffect;
+import backend.KadeEngineData;
+import backend.APIStuff;
+import backend.ConvertScore;
+import backend.CoolUtil;
 
-import options.Options;
-import options.PlayerSettings;
-import options.ButtonRemapSubstate;
-import options.GameplayCustomizeState;
+import options.Options;
+import options.PlayerSettings;
+import options.ButtonRemapSubstate;
+import options.GameplayCustomizeState;
 
-using StringTools;
+using StringTools;
