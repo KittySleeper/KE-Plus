1d9e0bf7e64302ebe4c0402da1ef0d4bc0cac029
Sox <113142608+Maz-github@users.noreply.github.com>
2024-07-19 18:06:16 -0400

    discord rpc!!!!

--- source/Discord.hx
+++ source/Discord.hx
@@ -12,7 +12,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "557069829501091850", // change this to what ever the fuck you want lol
+			clientID: "1263979245743640758", // change this to what ever the fuck you want lol
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
@@ -39,8 +39,8 @@ class DiscordClient
 		DiscordRpc.presence({
 			details: "In the Menus",
 			state: null,
-			largeImageKey: 'icon',
-			largeImageText: "fridaynightfunkin"
+			largeImageKey: 'FNF',
+			largeImageText: "Kade Plus"
 		});
 	}
 
